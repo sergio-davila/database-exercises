0047f48c3fadca8886cff02503e315801edb513f
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-19 11:29:54 -0500

    feat: finished select exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,3 +1,9 @@
 use codeup_test_db;
 
+SELECT 'All Pink Floyd albums' AS 'Info';
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT name FROM albums WHERE sales < 20.0;
+SELECT name FROM albums WHERE genre = 'Rock';
