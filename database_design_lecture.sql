1ce47e7ab8298fc7697d6cb466c6de9e700719ae
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-24 16:17:42 -0500

    feat: finished mostly with database design lecture

--- database_design_lecture.sql
+++ database_design_lecture.sql
@@ -36,3 +36,42 @@ values ((select id from authors where first_name = 'Kurt' and last_name = 'Vonne
         'The universe is a big place, perhaps the biggest.');
 INSERT INTO quotes (author_id, content)
 VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'), 'Don''t Panic.');
+
+-- select * from authors;
+-- select * from quotes;
+
+select authors.first_name,
+       authors.last_name,
+       quotes.content
+from quotes
+join authors on quotes.author_id = authors.id;
+
+-- select * from authors where first_name = 'Dogulas';
+-- update authors set first_name = 'Douglas' where id = 1;
+
+CREATE TABLE topics (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO topics(name) VALUES
+('Space and Time'),
+('Humor'),
+('Office Life'),
+('Hitchiker''s Guide to the Galaxy');
+
+CREATE TABLE quote_topic (
+     quote_id INTEGER UNSIGNED NOT NULL,
+     topic_id INTEGER UNSIGNED NOT NULL,
+     FOREIGN KEY (quote_id) REFERENCES quotes(id),
+     FOREIGN KEY (topic_id) REFERENCES topics(id)
+);
+
+INSERT INTO quote_topic(quote_id, topic_id)
+VALUES (1, 2), (1, 3), (2, 4), (3, 1),
+       (3, 2), (3, 3), (3, 4), (4, 2), (5, 1);
+
+select content, t.name from quotes
+join quote_topic qt on quotes.id = qt.quote_id
+join topics t on qt.topic_id = t.id;
